22a13806577ea705724bfba2a360eb5f01b67db8
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-28 12:09:41 -0300

    changed favicon and switched v-content to v-main

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <v-app>
         <app-header />
-        <v-content>
+        <v-main>
             <router-view />
-        </v-content>
+        </v-main>
         <Feedback />
     </v-app>
 </template>
