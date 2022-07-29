6d855378acbf44a5ba3330d16bf93865bc77f897
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-29 11:16:20 -0300

    separated book search into a new component

--- src/components/book/BookListPage.vue
+++ src/components/book/BookListPage.vue
@@ -13,60 +13,17 @@
                 </h6>
             </v-col>
         </v-row>
-        <v-row>
-            <v-col
-                cols="12"
-            >
-                <v-text-field
-                    v-model="textSearch"
-                    label="Pesquise Algo..."
-                    @change="searchBooks"
-                />
-            </v-col>
-        </v-row>
-        <v-row v-if="bookList.length > 0">
-            <v-col
-                v-for="(book, i) in bookList"
-                :key="i"
-                cols="12"
-                md="3"
-                lg="2"
-            >
-                <v-card class="mx-auto">
-                    <v-card-title>{{ book.volumeInfo.title }}</v-card-title>
-                    <v-card-text>{{ book.volumeInfo.subtitle }}</v-card-text>
-                    <v-card-actions>
-                        <v-btn
-                            text
-                            small
-                            color="primary"
-                        >
-                            Ver Detalhes
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
-        </v-row>
+        <book-list />
     </v-container>
 </template>
 
 <script>
-    import axios from 'axios';
+    import BookList from './BookList.vue';
 
     export default {
         name: 'BookListPage',
-        data() {
-            return {
-                bookList: [],
-                textSearch: '',
-            };
-        },
-        methods: {
-            searchBooks() {
-                axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.textSearch}`).then((response) => {
-                    this.bookList = response.data.items;
-                });
-            },
+        components: {
+            BookList,
         },
     };
 </script>
